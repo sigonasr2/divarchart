c9f4d9a136b4c66e0027b46cb0ffd5c13a106ac7
sigonasr2 <sigonasr2@gmail.com>
2020-10-01 06:09:01 +0900

    Create divarchart project.

---
+++ src/test/java/sig/chart/ChartApplicationTests.java
@@ -0,0 +1,13 @@
+package sig.chart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
